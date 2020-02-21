f9017506700afbabd92f60cf1022f9640afbc51d
Samantha Fuquay <samanthadeshong@yahoo.com>
2020-02-21 17:16:53 -0500

    class3 done

---
+++ Fuquay_Assignment3/fuquay_problem1/src/fuquay_problem1/CAI3.java
@@ -0,0 +1,157 @@
+package fuquay_problem1;
+
+	import java.security.SecureRandom;
+	import java.util.Scanner; 
+	//used geeksforgeeks.com to learn to use the random number generator 
+	public class CAI3 {
+		static Scanner scan = new Scanner(System.in);
+		public static void main(String[] args) {
+			quiz();
+			scan.close();
+		}
+
+		
+		public static void quiz() {
+			int correctans =0;
+	        for(int i =0; i<10; i++) {
+	        	SecureRandom rand = new SecureRandom(); 
+				int num_one = rand.nextInt(10); 
+		        int num_two = rand.nextInt(10);
+		        int correctAnswer = num_one * num_two;
+	        	askQuestion(num_one, num_two);
+	        	int studentAnswer= readResponse();
+	        	correctans =isAnswerCorrect(studentAnswer, correctAnswer, correctans);
+	        	
+	        }
+	        displayCompletionMethod(correctans);
+	        
+	        System.out.println("Please press one if you would like a new problem set or press 0 to exit");
+	        int help = scan.nextInt();
+	        if(help == 1) {
+	        	restartprogram();
+	        }
+	        else {
+	        System.exit(0);
+	        }
+		}
+		
+		public static void askQuestion(int num_one, int num_two) {
+			System.out.printf("What is %d times %d?\n", num_one, num_two);
+			
+		}
+		
+		public static int readResponse() {
+			int studentAnswer = scan.nextInt();
+			return studentAnswer;
+		}
+		
+		public static int isAnswerCorrect(int studentAnswer, int correctAnswer, int correctans) {
+			if(studentAnswer == correctAnswer) {
+				displayCorrectResponse();
+				correctans ++;
+				return correctans;	
+			}
+			else {
+				displayIncorrectResponse();
+				return correctans;
+			}
+		}
+		
+		public static void displayCorrectResponse(){
+			SecureRandom rand = new SecureRandom(); 
+			int snum = rand.nextInt(4)+1; 
+			switch(snum) {
+			case 1:
+				System.out.println("Very good!");
+				break;
+			case 2:
+				System.out.println("Excellent!");
+				break;
+			case 3:
+				System.out.println("Nice work!");
+				break;
+			case 4:
+				System.out.println("Keep up the good work!");
+				break;
+			default:
+				System.out.println("System malfunction please try again");
+				break;
+			
+			}
+		}
+		
+		public static void displayIncorrectResponse(){
+			SecureRandom rand = new SecureRandom(); 
+			int snum = rand.nextInt(4)+1; 
+			switch(snum) {
+			case 1:
+				System.out.println("No. please try agian!");
+				break;
+			case 2:
+				System.out.println("Wrong. Try once more.");
+				break;
+			case 3:
+				System.out.println("Don't give up!");
+				break;
+			case 4:
+				System.out.println("No. Keep trying.");
+				break;
+			default:
+				System.out.println("System malfunction please try again");
+				break;
+			
+			}
+		}
+		public static void displayCompletionMethod(int correctans){
+			float percentcorrect = (correctans / 10) * 100;
+			if(percentcorrect < 75) {
+				System.out.println("Please ask your teacher for extra help.");
+			}
+			else {
+				System.out.println("Congratulations, you are ready to go to the next level!");
+			}
+		}
+		public static void restartprogram() {
+			quiz();
+		}
+
+}
+	/*
+	Modify the program from Part 2 to use your question generation method to ask the student
+	10 different questions. Give students only one chance at answering each question. 
+	Count the number of correct and incorrect responses typed by the student. 
+	After the program has asked 10 questions (and the student types 10 answers), 
+	your program should calculate the percentage that are correct. If the percentage is lower 
+	than 75%, display "Please ask your teacher for extra help.", then reset the program
+	so another student can try it. If the percentage is 75% or higher
+	, display "Congratulations, you are ready to go to the next level!", then reset 
+	the program so another student can try it.
+	
+	Multiplication problems shall contain two numbers sampled from a
+	 uniform random distribution in the range of 0 to 9 (inclusive)
+	The program shall display a random positive message if the student provides a correct response
+	The program shall display a random negative message if the student provides an incorrect response
+	The program shall display the student's score after the student has attempted to solve 10 problems
+	The student's score shall be the percentage of problems correctly solved
+	The program shall display the message "Please ask your teacher for extra help." if 
+	the student's score is less than 75%
+	The program shall display the message "Congratulations, you are ready to go to the next level!" 
+	if the student's score is greater than or equal to 75%
+	The program shall ask the student if they want to solve a new problem set 
+	after the score message has been displayed
+	The program shall restart when the student agrees to solve a new problem set
+	The program shall terminate when the student declines to solve another problem set
+	Create a method called "quiz" that contains the program logic
+	Create a function called "askQuestion" that prints the problem to the screen
+	Create a function called "readResponse" that reads the answer from the student
+	Create a function called "isAnswerCorrect" that checks to see if the student's 
+	answer matches the correct answer to the problem
+	Create a function called "displayCorrectResponse" that prints out the 
+	response when a student enters a correct answer
+	Create a function called "displayIncorrectResponse" that prints out the response 
+	when a student enters an incorrect answer
+	Create a function called "displayCompletionMessage" that prints out the studen't 
+	score and appropriate score response
+	Create a main method that runs your program by calling the "quiz" method
+	 */
+	
